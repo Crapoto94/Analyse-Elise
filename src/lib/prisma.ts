c986c2553f55b8dff9b6964b207f4886e8e5f258
CHEVALIER Marc <MaChevalier@ivry94.fr>
2026-03-21 00:49:34 +0100

    Mise à jour depuis local

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,8 @@
 import { PrismaClient as SystemClient } from '../../node_modules/@prisma/client/system'
 import { PrismaClient as EntitiesClient } from '../../node_modules/@prisma/client/entities'
 
-const prismaSystem = new SystemClient({
-  datasources: { db: { url: process.env.DATABASE_URL_SYSTEM } }
-})
-const prismaEntities = new EntitiesClient({
-  datasources: { db: { url: process.env.DATABASE_URL_ENTITIES } }
-})
+const prismaSystem = new SystemClient()
+const prismaEntities = new EntitiesClient()
 
 export { prismaSystem, prismaEntities }
 export default prismaSystem // Default to system for convenience in some parts
